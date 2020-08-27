3eb4075b9318e0e92d07e0e37cc079db00b15e63
jong in <bill1224@naver.com>
2020-08-27 14:33:06 +0900

    마이페이지 기능 추가

--- curture_page/page/mypage.php
+++ curture_page/page/mypage.php
@@ -33,50 +33,13 @@
   <script src="../header/js/bootstrap.min.js"></script>
   <!-- <script src="../home/js/main.js" defer></script> -->
 
+  <!-- mypage의 css -->
+  <link rel="stylesheet" href="./css/mypage_css.css">
   <!--고정 headere-->
   <?php include "../header/header.php"; ?>
-
-  <style>
-    .user_id > span{
-      text-decoration:underline;
-      font-size:0.9em;
-    }
-    .container{
-      /* margin-top:40px; */
-      display:flex;
-    }
-    .total_button{
-      margin-right:40px;
-    }
-    .table_title{
-      padding:32px;
-    }
-    .table_content{
-      padding: 32px 0px 32px 30px;      
-    }
-    .list-group{
-      margin-top:40px;
-      margin-right:40px;
-    }
-    .user_info{
-      margin-top:40px;
-    }
-  </style>
 </head>
-
 <body>
 <div class="container"> 
-  <!-- <div class="total_button">
-    <div class="user_button">
-      <button type="button" class="btn btn-info" onclick="location.href='./mypage.php?page=1' ">회원정보 변경</button>
-    </div>    
-    <div class="bookmark_button">
-      <button type="button" class="btn btn-info" onclick="location.href='mypage.php?page=2' ">회원탈퇴</button>
-    </div>
-    <div class="bookmark_button">
-      <button type="button" class="btn btn-info" onclick="location.href='mypage.php?page=3' ">즐겨찾기 목록보기</button>
-    </div>
-  </div>  -->
   <div class="list-group">
   <button type="button" class="list-group-item list-group-item-action active" onclick="location.href='./mypage.php' ">
     마이페이지
@@ -88,14 +51,17 @@
 </div>
   
 <?php
+//각 항목마다 url에 page 번호를 매겨서 현재 페이지에서 url에 따라서 내용물만 변하게 하기위해서 조건을 각각 주었습니다.
+//mypage의 초기 화면일 경우
 if(!isset($_GET['page'])){
+  //회원의 개인정보들을 보여주기위해서 db에서 호출
   $sql = mq("select * from members where id = '".$_SESSION['id']."'");
   $result = $sql -> fetch_array();
   $fillarray = get_array_user($result);
+  //항목마다 테이블을 만들면 코딩이 너무 길어져서  반복문을 사용하기 위해서 배열에 넣었다.
   $user_field = ['사용자 이름','생일','성별','계정 이메일'];
-  $user_content = ['name','birthday','gender','email'];
+  $user_content = ['name','birthday','gender','email']; ?>
 
-?>
 <div class ="user_info">
   <div class="user_id">
     <strong><?=$_SESSION['id']?></strong> 님의 정보입니다.<br>
@@ -105,18 +71,12 @@ if(!isset($_GET['page'])){
     <table class="table table-striped">
       <tr height="150px">
         <th width="200px">
-          <div class="table_title">
-            등급
-          </div>
+          <div class="table_title">등급</div>
         </th>
         <td>
           <div class="table_content">
-            <div>
-              <?=$fillarray['rank']?>
-            </div>
-            <div>
-              <small>홈페이지에서 많이 활동하면 등급을 올려서 더 많은 혜택을 받을 수 있어요~!.</small>
-            </div>
+            <div><?=$fillarray['rank']?></div>
+            <div><small>홈페이지에서 많이 활동하면 등급을 올려서 더 많은 혜택을 받을 수 있어요~!.</small></div>
           </div>
         </td>
       </tr>
@@ -130,41 +90,87 @@ if(!isset($_GET['page'])){
         </th>
         <td>
           <div class="table_content">
-            <div>
-              <?=$fillarray[$user_content[$i]]?>
-            </div>
-            <div>
-              <small>이름이나 생년월일, 성별 등의 정보가 변경되었다면 본인확인을 통해 정보를 수정할 수 있습니다.</small>
-            </div>
-            <div>
-              수정
-            </div>
+            <div><?=$fillarray[$user_content[$i]]?></div>
+            <div><small>이름이나 생년월일, 성별 등의 정보가 변경되었다면 본인확인을 통해 정보를 수정할 수 있습니다.</small></div>
+            <div>수정</div>
           </div>
         </td>
       </tr>
       <?php }?>
-
     </table>
   </div>
 </div>
 
 <?php } 
+  // 회원탈퇴를 눌렀을 경우의 페이지  
   else if($_GET['page']==1){ ?>
-  <div class="">
+  <div class="user_drop">
     <form action="../login/check/user_drop.php" method="post">
-      <div >
+      <div class="drop_title">
         회원탈퇴
       </div>
-      <div>
-        비밀번호를 입력해주세요.
-        <input type="password" name="password">
+      <div class="user_input">        
+        <input type="password" name="password" placeholder="비밀번호를 입력해주세요.">
         <input type="submit" value="확인">      
       </div>
     </form>
   </div>
 
+<?php }
+  //자신이 즐겨찾기를 누른 게시글들을 볼 수 있는 페이지 
+  else if($_GET['page']==2){?>
+  <div class="">
+    <?php
+    // 현재 사용자의 즐겨찾기 목록을 가져오기 위해서 bookmark table과 members table을 조인해서 db를 가져온다
+    $sql = mq("select * from members_bookmark as book, members as mem where book.membersNum = mem.mem_no and id = '".$_SESSION['id']."'");
+    $result = $sql -> fetch_array();
+    //즐겨찾기 목록을 string 형태로 다 가져옮
+    $array = $result['reviewNum'];
+    // explode(); 는 string 형태의 data를  ','라는 특수문자를 기준으로 나눠서 배열로 만들어준다
+    $bookmark_array = explode(',', $array);
+    ?>
+    <div class="bookmark_list">
+      <table class="table table-striped">
+    <?php
+    //count($bookmark_array) 를 통해서 배열의 갯수만큼 반복시켜서 모든 즐겨찾기를 가져올 수 있도록
+    for($i=0; $i<count($bookmark_array); $i++){  
+      $sql2 = mq("select * from review where review_no ='".$bookmark_array[$i]."'");
+      $result2 = $sql2 -> fetch_array(); ?>
+        <tr>
+          <td>
+            <!-- 즐겨찾기 번호에 해당하는 title을 보여주고, 링크로 해당하는 url review_no로 보내주면 해당 게시글로 바로 이동한다 -->
+            <a href="./japan_review_read.php?review_no=<?=$bookmark_array[$i]?>"><?=$result2['title']?></a>      
+          </td> 
+        </tr>
+    <?php }?>
+      </table>  
+    </div>
+  </div>
+<?php }
+  //자신이 게시한 글을 볼 수 있는 페이지
+  else if($_GET['page']==3){?>
+   <div class="">
+    <?php
+    // 사용자의 id로 작성되어있는 모든 글을 가져온다 .
+    $sql = mq("select * from review where id ='".$_SESSION['id']."'");
+    $low = $sql -> num_rows;
+    ?>
+    <div class="myReview_list">
+      <table class="table table-striped">
+    <?php
+    for($i=0; $i<$low; $i++){  
+      $result = $sql -> fetch_array(); ?>      
+        <tr>
+          <td>
+            <!-- for문을 통해 순차대로 db를 가져온는데 해당하는 title 을 누르면 링크로 해당하는 url review_no 로 보내줘서 바로 게시글로 이동-->
+            <a href="./japan_review_read.php?review_no=<?=$result['review_no']?>"><?=$result['title']?></a>      
+          </td> 
+        </tr>
+    <?php }?>
+      </table>  
+    </div>
+  </div>
 <?php }?>
-
 </div>
 </body>
 </html>
--- curture_page/page/review/check/modify_ok.php
+++ curture_page/page/review/check/modify_ok.php
@@ -6,17 +6,19 @@
   $review_no = htmlspecialchars ($_POST['review_no'], ENT_QUOTES);
 
   if($title == null || $description == null){
-    echo "<script>alert('내용을 모두 입력해주세요.'); location.href='../../japan_review_modify.php?review_no='".$review_no."'';</script>";
+    echo "<script>alert('내용을 모두 입력해주세요.'); location.href='../../japan_review_modify.php?review_no=$review_no';</script>";
     exit;
   } else {
     $sql = mq("update review set title ='".$title."' where review_no = '".$review_no."'");
     $sql2 = mq("update review set description ='".$description."' where review_no = '".$review_no."'");
   }
+  
 
     if($sql && $sql2){
-      echo "<script>alert('수정 성공했습니다.'); location.href='../../japan_review_read.php?review_no='".$review_no."'';</script>";
+      echo "<script>alert('수정 성공했습니다.'); location.href='../../japan_review_read.php?review_no=$review_no';</script>";
+      exit;
     } else {
-      echo "<script>alert('수정에 실패했습니다.'); location.href='../../japan_review_modify.php?review_no='".$review_no."'';</script>";
+      echo "<script>alert('수정에 실패했습니다.'); location.href='../../japan_review_modify.php?review_no=$review_no';</script>";
     }
   
 
