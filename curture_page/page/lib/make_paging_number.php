5f52b181a4fae1ae072c195c699b67a2cdbc01a9
jong in <bill1224@naver.com>
2020-08-30 18:46:14 +0900

    iframe 메소드 수정

--- curture_page/page/lib/make_paging_number.php
+++ curture_page/page/lib/make_paging_number.php
@@ -4,37 +4,44 @@ function make_paging_number($i, $search_title, $genre, $kategorie){
   switch($i){
     // 카테고리 x 장르 x 검색 o 일 경우
     case 1:      
-    $sql = mq("SELECT * FROM review WHERE title LIKE '%{$search_title}%'");
+      $sql = mq("SELECT * FROM review WHERE title LIKE '%{$search_title}%'");
 
+      break;
     // 카테고리 x 장르 x 검색 x 일 경우
     case 2:      
       $sql = mq("SELECT * FROM review");
-    
+
+      break;
     // 카테고리 x 장르 o 검색 o 일 경우
     case 3:      
       $sql = mq("SELECT * FROM review WHERE genre = '{$genre}' and title LIKE '%{$search_title}%'");
 
-    
+      break;
     // 카테고리 x 장르 o 검색 x 일 경우
     case 4:
       $sql = mq("SELECT * FROM review WHERE genre = '{$genre}'");
 
+      break;
     // 카테고리 o 장르 x 검색 o 일 경우
     case 5:      
       $sql = mq("SELECT * FROM review WHERE kategore = '{$kategore}' and title LIKE '%{$search_title}%'");
 
+      break;
     // 카테고리 o 장르 x 검색 x 일 경우
     case 6:      
       $sql = mq("SELECT * FROM review WHERE kategorie = '{$kategorie}'");
 
+     break;
     // 카테고리 o 장르 o 검색 o 일 경우  
     case 7:      
       $sql = mq("SELECT * FROM review WHERE kategore = '{$kategore}' and genre = '{$genre}' 
                 and title LIKE '%{$search_title}%'");
-
+      break;
     // 카테고리 o 장르 o 검색 x 일 경우
     case 8:      
       $sql = mq("SELECT * FROM review WHERE kategore = '{$kategore}' and genre = '{$genre}'");
+    
+      break;
   }
   //한페이지에 나올 view 수 
   $numView = 10;
